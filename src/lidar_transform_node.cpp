2edfd55c7918f2dd57aa3a42269c8d2667b480a4
hjh6230 <hjh6230lance@gmail.com>
2017-10-05 15:56:35 +0200

    tf created

--- src/lidar_transform_node.cpp
+++ src/lidar_transform_node.cpp
@@ -1,13 +1,30 @@
 #include <ros/ros.h>
 #include <ras_group8_transform/LidarTransform.hpp>
+#include <tf/transform_broadcaster.h>
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "ras_group8_lidar_transform");
   ros::NodeHandle nodeHandle("~");
 
-  ras_group8_template::LidarTransform mainObject(nodeHandle);
+  ras_group8_transform::LidarTransform mainObject(nodeHandle);
 
-  ros::spin();
-  return 0;
+
+
+  tf::TransformBroadcaster br;
+  tf::Transform transform;
+   
+ ros::Rate rate(10.0);
+ while (nodeHandle.ok()){
+   transform.setOrigin( tf::Vector3(5.0, 0.0, 0.0) );//need to measure the true distance
+   transform.setRotation( tf::Quaternion(0, 0, 0, 1) );
+   br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "robot", "laser"));
+   rate.sleep();
+ }
+ return 0;
+
+
+
+  // ros::spin();
+  // return 0;
 }
